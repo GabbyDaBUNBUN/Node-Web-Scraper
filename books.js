d02bb4cbd88a632ed88d38193fe7943fea776003
GabbyDaBUNBUN <bunbun@expressionbot.xyz>
2023-08-13 21:47:30 -0500

    update to store books data

--- books.js
+++ books.js
@@ -1,5 +1,7 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const j2cp = require('json2csv').Parser;
+const fs = require('fs');
 
 const url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/index.html';
 const baseUrl = 'https://books.toscrape.com/catalogue/category/books/mystery_3/'
@@ -18,6 +20,10 @@ async function getBooks(url) {
             if ($(".next a").length > 0) {
                 nextPage = baseUrl + $(".next a").attr("href");
                 getBooks(nextPage)
+            } else {
+                const parser = new j2cp();
+                const csv = parser.parse(bookData);
+                fs.writeFileSync("./books.csv", csv);
             }
         });
         console.log(bookData);
